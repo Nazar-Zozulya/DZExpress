91001f92cc0b95dc8e26feb5cc3d0e45154fee34
Nazar Zozulya <likeemangames@gmail.com>
2024-10-17 21:08:30 +0300

    DZ1710

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -58,6 +58,108 @@ async function deleteOnePost(){
     })
 }
 
+
+
+
+
+
+
+
+async function createOneComment(){
+    const comment = await prisma.comment.create({
+        data: {
+            comment: 'First comment',
+            title: 'First title',
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmRtCXZnOIjV-MQkeAjjoPd2_SHPmTzJur2A&s',
+            postId: 1
+        }
+    })
+}
+
+async function createManyComments(){
+    const comment = await prisma.comment.createMany({
+        data: [
+            {
+                comment: 'Second comment',
+                title: 'Second title',
+                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmRtCXZnOIjV-MQkeAjjoPd2_SHPmTzJur2A&s',
+                postId: 1
+            },
+            {
+                comment: 'Third comment',
+                title: 'Third title',
+                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmRtCXZnOIjV-MQkeAjjoPd2_SHPmTzJur2A&s',
+                postId: 2
+            },
+            {
+                comment: 'Fourth comment',
+                title: 'Fourth title',
+                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmRtCXZnOIjV-MQkeAjjoPd2_SHPmTzJur2A&s',
+                postId: 2
+            },
+        ]
+    })
+}
+
+async function deleteOneComment(){
+    const comment = await prisma.comment.delete({
+        where: {
+            id: 2,
+        }
+    })
+}
+
+async function findOneComment(){
+    const comment = await prisma.comment.findUnique({
+        where: {
+            id: 1,
+        }
+    })  
+}
+
+async function findOneCommentByPost(){
+    const comment = await prisma.comment.findUnique({
+        where: {
+            id: 1,
+        },
+
+        include: {
+            post :true
+        }
+    })  
+}
+
+async function findPostByComment(){
+    const post = await prisma.post.findMany({
+        where: {
+            id: 1,
+        },
+
+        include: {
+            comments : true
+        }
+    })
+}
+
+async function updateOneComment(){
+    const comment = await prisma.comment.update({
+        where: {
+            id: 1
+        },
+
+        data: {
+            comment: 'Updated comment',
+            title: 'Updated title',
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmRtCXZnOIjV-MQkeAjjoPd2_SHPmTzJur2A&s',
+        }
+    })
+}
+
+
+
+
+
+
 async function main() {
     await createOnePost()
     await createManyPosts()
@@ -65,7 +167,15 @@ async function main() {
     await getOnePost()
     await getManyPosts()
     await deleteOnePost()
+    await createOneComment()
+    await createManyComments()
+    await deleteOneComment()
+    await findOneComment()
+    await findOneCommentByPost
+    await findPostByComment()
+    await updateOneComment()
 }
+
 
 main().then(() => {
     prisma.$disconnect()
